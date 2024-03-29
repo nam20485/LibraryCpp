8b86bc1f51c95ee4f7b11d9515363a0003a41a1b
Nathan Miller <nmiller217@gmail.com>
2024-03-29 09:22:27 -0700

    Add project files.

---
+++ Driver/LibraryCpp.cpp
@@ -0,0 +1,12 @@
+﻿// LibraryCpp.cpp : Defines the entry point for the application.
+//
+
+#include "LibraryCpp.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
